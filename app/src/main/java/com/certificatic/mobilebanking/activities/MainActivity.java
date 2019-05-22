23b2c4e0997367dd8cc10f64b755e35bc8268444
liviso <mendozalivs@gmail.com>
2019-05-21 22:41:04 -0500

    first structure

---
+++ app/src/main/java/com/certificatic/mobilebanking/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.certificatic.mobilebanking.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.certificatic.mobilebanking.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
